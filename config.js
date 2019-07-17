0dc62dac9032f508aa0e921987b427a4dd87ccba
abrentgit <abprojects.ca@gmail.com>
2019-07-17 16:53:17 -0400

    initial

---
+++ config.js
@@ -0,0 +1,15 @@
+'use strict';
+
+exports.DATABASE_URL =
+  process.env.DATABASE_URL ||
+  global.DATABASE_URL ||
+  'mongodb://localhost:27017/Goodnest';
+
+exports.TEST_DATABASE_URL =
+  process.env.TEST_DATABASE_URL || 'mongodb://localhost:27017/Goodnest-api';
+exports.PORT = process.env.PORT || 8080;
+
+exports.JWT_SECRET = process.env.JWT_SECRET || 'sugarlandmalibu19';
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:8080/';
